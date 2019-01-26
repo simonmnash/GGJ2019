129dd70f159800605cb53fcb168f7a69e3695ef7
David French <dqf318@yahoo.com>
2019-01-26 17:34:48 -0500

    Menus

---
+++ House.gd
@@ -0,0 +1,15 @@
+extends Node
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://Menus/Ending.tscn")
