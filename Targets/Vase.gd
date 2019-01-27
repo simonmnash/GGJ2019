c60dfae0dab8af43380ce0d0a6f548d1bb6ccd4b
David French <dqf318@yahoo.com>
2019-01-27 13:56:21 -0500

    Target Fix

--- Targets/Vase.gd
+++ Targets/Vase.gd
@@ -1,21 +1,10 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
+var knockedover = false
 
 func _on_Area2D_area_entered(area):
-	self.move_local_y(190)
-	$AnimatedSprite.play("broken")
-	self.z_index = 0
+	if knockedover == false:
+		self.move_local_y(190)
+		$AnimatedSprite.play("broken")
+		self.z_index = 0
+		knockedover = true
